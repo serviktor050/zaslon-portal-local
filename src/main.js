f4b56123507f2cc576785d168cf8910283a5c07f
sergei-gagulin <gagulin.s@triadasite.ru>
2026-03-18 18:34:32 +0300

    ZS-65 Работы по блоку Отзывы о нашей работе

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
 import './style.scss'
 
 console.log('Главная страница');
+
+const swiper = new Swiper('.reviews__body', {
+    modules: [Navigation],
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    loop: true,
+    loopedSlides: 7,
+    slideToClickedSlide: true,
+    spaceBetween: 20, // небольшой отступ между слайдами
+
+    navigation: {
+        nextEl: '.next-btn',
+        prevEl: '.prev-btn',
+    },
+});
