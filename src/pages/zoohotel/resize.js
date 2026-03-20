7b05599a4f49112d46e9d57114e32aacb3c42ff2
sergei-gagulin <gagulin.s@triadasite.ru>
2026-03-20 12:12:48 +0300

    ZS-65 Работаю над созданием отдельных страниц

--- src/pages/zoohotel/resize.js
+++ src/pages/zoohotel/resize.js
@@ -1,4 +1,4 @@
-import { throttle } from './throttle'
+import { throttle } from './throttle.js'
 
 const widthListeners = new Set()
 const heightListeners = new Set()
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
+                zoo: resolve(__dirname, 'zoo.html'),
             },
         },
     },
